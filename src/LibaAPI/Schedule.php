8be3b6ce3ab636f7d05c8c72d6e98723620b3919
Tommy Ku <zoo1929@outlook.com>
2015-03-18 00:58:18 +0800

    ADD: Schedule class isAvailableWithoutLogin()

--- src/LibaAPI/Schedule.php
+++ src/LibaAPI/Schedule.php
@@ -19,6 +19,20 @@ class Schedule
         $this->validateDate($date);
     }
 
+    public function isAvailableWithoutLogin($start, $end, $area, $room)
+    {
+        $this->validateBookingDuration($start, $end);
+        $schedule = Parser::parseRoom($start, $area, $room);
+        $startTimeslot = $this->dateTime2timeslot($start);
+        $endTimeslot = $this->dateTime2timeslot($end);
+        foreach ($schedule['timeslots'] as $t) {
+            if ($startTimeslot >= $t['start'] && $endTimeslot <= $t['end']) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public function isAvailableWithLogin($start, $end, $area, $room)
     {
         if ($this->bAuth_user === '' || $this->bAuth_pass === '') {
@@ -97,6 +111,11 @@ class Schedule
         return intval($date->format('G'))*3600+intval($date->format('i'))*60+intval($date->format('s'));
     }
 
+    private function dateTime2timeslot($date)
+    {
+        return (intval($date->format('G'))*2 + (($date->format('i') == '00') ? 0 : 1));
+    }
+
     private function validateDate($date)
     {
         $date->modify('midnight');
@@ -105,6 +124,19 @@ class Schedule
 
         if ($date < $dateHead || $date >= $dateTail) {
             throw new Exceptions\InvalidDateException('Date outside acceptable range');
+        }
+    }
+
+    private function validateBookingDuration($start, $end)
+    {
+        if ($end <= $start) {
+            throw new Exceptions\InvalidDateException('End date time should be earlier than start date time');
+        }
+
+        $start = (clone $start);
+        $start->modify('+2 hours');
+        if ($end > $start) {
+            throw new Exceptions\InvalidDateException('Booking duration should be shorter or equal to 2 hours');
         }
     }
 
